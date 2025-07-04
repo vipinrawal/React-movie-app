4e36c70f6eb375e634ad6726198919de37b6b4ab
vipin <86299966+vipinrawal@users.noreply.github.com>
2025-07-04 21:54:46 +0530

    Update axios.jsx

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -4,7 +4,7 @@ const instance = axios.create({
     baseURL:"https://api.themoviedb.org/3/",
     headers:{
         accept: 'application/json',
-        Authorization: process.env.Api_key
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNGExYjBkZmNjZjhkZGM1MWZhMGE1NDBhYzE1ZmYzYyIsIm5iZiI6MTc1MDkzOTYwMi44MzYsInN1YiI6IjY4NWQzN2QyYzA2NmE0OTRjMTM0ZjAzNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.k-ylHtWC95iq4-9F_G7dlifdv3BAdSim7ApT1ag4AhI'
     }
 })
 
